699bbaccee745711383e98d72215113db84a99a4
nadinestrella <nadinestrella@gmail.com>
2025-10-02 15:23:02 +0200

    added route to detail

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import Home from '@/views/Home.vue';
 import About from '@/views/About.vue';
+import Detail from '@/views/Detail.vue';
 
 const routes = [
   { path: '/', name: 'Home', component: Home },
   { path: '/about', name: 'About', component: About },
+  { path: '/detail', name: 'Detail', component: Detail },
 ];
 
 const router = createRouter({
---
+++ src/views/Detail.vue
@@ -0,0 +1,3 @@
+<script></script>
+
+<template>Soy un detail</template>
